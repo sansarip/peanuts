5130a5f21d833c9e9749073e4a681e49ace799ba
sepehr ansaripour <pehrans@gmail.com>
2020-06-01 17:05:11 -0400

    Update project version and description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject peanuts "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject peanuts "0.1.0"
+  :description "Packing peanut macros for Reagent/Re-frame Form-1 components"
+  :url "https://github.com/sansarip/peanuts"
   :source-paths ["src/cljc"]
   :test-paths ["test/cljc"]
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"

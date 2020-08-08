effc37997cb77588eb105a405789b4b86e585f47
sepehr ansaripour <pehrans@gmail.com>
2020-08-08 16:16:53 -0400

    Update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject peanuts "0.3.4.1"
+(defproject peanuts "0.3.5"
   :description "Packing peanuts for decoupling Reagent Form-1 components from Re-frame subscriptions"
   :url "https://github.com/sansarip/peanuts"
   :source-paths ["src/cljc"]

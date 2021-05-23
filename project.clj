9f7022be09cd07b3731715199779d92987f74049
sansarip <pehrans@gmail.com>
2021-05-23 17:56:09 -0400

    🔧 Add signing key

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject peanuts "0.4.0"
   :description "Packing peanuts for decoupling Reagent Form-1 components from Re-frame subscriptions"
   :url "https://github.com/sansarip/peanuts"
+  :signing {:gpg-key "pehrans@gmail.com"}
   :source-paths ["src/cljc" "src/cljs"]
   :test-paths ["test/cljc"]
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"

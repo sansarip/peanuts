9bde8ec02df90f17dcaaa83cf44c314a4b814bbc
sansarip <pehrans@gmail.com>
2021-06-09 10:25:28 -0400

    ➕ Add compile namespace to evaluate cljs

---
+++ devcards/cljs/peanuts/compile.cljs
@@ -0,0 +1,43 @@
+(ns peanuts.compile
+  (:require
+    [peanuts.macros :as pm :refer-macros [literally]]
+    [cljs.js :as cljs-js]))
+
+;; Copied from https://github.com/ctford/klangmeister/blob/a9cdae2c3d0b71e458576fb693f5cf4654ba73e9/src/klangmeister/compile/eval.cljs
+;; Thanks :-)
+
+(def dependencies
+  "A bundle of dependencies."
+  (pm/sources
+    clojure.set
+    clojure.walk
+    peanuts.core))
+
+(def namespace-declaration
+  (literally
+    (ns cljs.user
+      (:require-macros [peanuts.core :refer [defnc fnc]]))))
+
+(defn loader
+  "A namespace loader that looks in the dependencies bundle for required namespaces."
+  [{:keys [name]} callback]
+  (let [str-name (.-str name)
+        source (dependencies str-name)]
+    (if source
+      (js/console.log (str "Loading " str-name))
+      (js/console.log (str "Unable to load " str-name)))
+    (callback {:lang :clj :source (str source)})))
+
+(def state
+  "A compiler state, which is shared across compilations."
+  (cljs-js/empty-state))
+
+(defn evaluate
+  [s cb]
+  (cljs-js/eval-str
+    state
+    (str namespace-declaration s)
+    nil
+    {:eval cljs-js/js-eval
+     :load loader}
+    cb))

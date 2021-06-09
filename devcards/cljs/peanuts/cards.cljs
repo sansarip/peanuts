d2ddae303739ca62cb076806d74b3661470db0b2
sansarip <pehrans@gmail.com>
2021-06-09 10:29:33 -0400

    💄 Begin to add examples

--- devcards/cljs/peanuts/cards.cljs
+++ devcards/cljs/peanuts/cards.cljs
@@ -3,40 +3,80 @@
     [devcards.core :as dc]
     [re-frame.core :as rf]
     [re-frame.db :refer [app-db]]
-    [reagent.core :as r]
-    [peanuts.macros :refer [defcard-ide]]
-    [peanuts.core :as pn]
-    [peanuts.editor :refer [ide]]))
+    [peanuts.ide :as ide]
+    [peanuts.macros :as pm]
+    [peanuts.compile :as compile]
+    [peanuts.core :refer-macros [defnc fnc]]))
 
 (enable-console-print!)
 
 (reset! app-db {:name "World"})
 
+(rf/reg-sub :name
+  (fn [db] (:name db)))
+
 (rf/reg-sub
   ::name
   (fn [{n :name}] n))
-#_(defcard-rg+
-    defnc
-    "Simple defnc usage, normally the defnc wouldn't be nested in a function \uD83D\uDE0A"
-    (fn []
-      (pn/defnc greeting [n]
-        [:div (str "Hello, " n "!")])
-      [:<>
-       [greeting "Garbonzo"]
-       [greeting ::name]]))
 
-(defcard-ide
-  editor
-  ""
-  ide
+(defn ide-with-eval [default-input]
+  [:> ide/ide
+   {:eval-fn       compile/evaluate
+    :default-input default-input}])
+
+(dc/defcard-rg
+  ex1-form-1-components
+  "Here's how form-1 components are usually defined and used"
   (fn []
-    #_#_#_
-    (require-macros '[peanuts.core :refer [defnc]])
-    (defnc greeting [n] n)
-    (greeting 1)
-    (require '[peanuts.editor :refer [hiccup?]])
-    (hiccup? [:a])))
+    [ide-with-eval
+     (pm/literally
+       (defn greeting []
+         (let [n @(re-frame.core/subscribe [:name])]
+           [:p "Hello, " n "!"]))
+       [:div
+        [:b
+         "This greeting component is not a pure function. "
+         "As a result, the component is harder to reuse into other projects, "
+         "and it's more difficult to test."]
+        [greeting]])]))
 
+(dc/defcard-rg
+  ex2-form-1-components
+  "A solution is to separate the logic and state!"
+  (fn []
+    [ide-with-eval
+     (pm/literally
+       (defn greeting* [n]
+         [:p "Hello, " n "!"])
+       (defn greeting []
+         (let [n @(re-frame.core/subscribe [:name])]
+           [greeting* n]))
+       [:div
+        [:b
+         "This works, at the cost of wrapping every component with another "
+         "\uD83D\uDE05\uD83E\uDD37"]
+        [greeting]])]))
+
+(dc/defcard-rg
+  ex3-defnc
+  "In contrast to ex2, note how the `greeting` component looks like a pure function without any additional ceremony"
+  (fn []
+    [ide-with-eval
+     (pm/literally
+       (defnc greeting [n]
+         [:p "Hello, " n "!"])
+       [:div
+        [:b
+         "If passing in a keyword, then that keyword will be used to look up a subscription"]
+        [greeting :name]
+        [:b
+         "Same component, but now we're passing in a value instead of a subscription id!"]
+        [greeting "Garbonzo"]
+        [:i
+         "Because the component and the re-frame state are decoupled, "
+         "now you can reuse this component in another project. "
+         "The component is also much easier to test because you can just pass in "
+         "raw values \uD83D\uDE4C"]])]))
 
 (defn ^:export init []
   (dc/start-devcard-ui!))

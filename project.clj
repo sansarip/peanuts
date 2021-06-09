c2ff21e8f3b12436a557c2cf85fd9571d95a5d6c
sansarip <pehrans@gmail.com>
2021-06-09 18:43:03 -0400

    🔧 Remove unnecessary clean target

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/sansarip/peanuts"
   :signing {:gpg-key "pehrans@gmail.com"}
   :test-paths ["test/cljc"]
-  :clean-targets ^{:protect false} ["resources/public/dev/js/compiled" "resources/public/js/compiled" "target"]
+  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]

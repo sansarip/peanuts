7461e8ac120550264d602dd4b516d5b9bd43e82d
sepehr ansaripour <pehrans@gmail.com>
2021-06-18 01:53:45 -0400

    :wrench: Separate out source paths interfering with devcards

--- project.clj
+++ project.clj
@@ -9,20 +9,21 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/test.check "1.0.0"]]
   :repl-options {:init-ns peanuts.core}
-  :source-paths ["src/cljc"]
+  :source-paths []
   :aliases {"fig:prod"       ["with-profile" "devcards" "run" "-m" "figwheel.main" "-bo" "prod"]
             "fig:dev"        ["with-profile" "devcards" "trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
-            "deploy:clojars" ["with-profile" "peanuts" "deploy" "clojars"]}
-  :profiles {:test     {:source-paths ["src/cljc" "src/cljs" "test/deps"]}
-             :peanuts  {:source-paths ["src/cljc" "src/cljs"]}
-             :devcards {:source-paths ["devcards/src/cljs" "devcards/src/cljc"]
-                        :dependencies [[com.bhauman/figwheel-main "0.2.13"]
-                                       [org.clojure/tools.namespace "1.1.0"]
-                                       [org.clojure/clojurescript "1.10.866"]
-                                       [re-frame "1.2.0"]
-                                       [reagent "0.8.1"]
-                                       [re-catch "0.1.4"]
-                                       [devcards "0.2.6"]
-                                       [cljstache "2.0.6"]
-                                       [net.dhleong/spade "1.0.4"]]}})
+            "deploy:clojars" ["with-profile" "peanuts" "deploy" "clojars"]
+            "test"           ["with-profile" "peanuts,test-deps" "test"]}
+  :profiles {:peanuts   {:source-paths ["src/cljc" "src/cljs"]}
+             :test-deps {:source-paths ["test/deps"]}
+             :devcards  {:source-paths ["devcards/src/cljs" "devcards/src/cljc"]
+                         :dependencies [[com.bhauman/figwheel-main "0.2.13"]
+                                        [org.clojure/tools.namespace "1.1.0"]
+                                        [org.clojure/clojurescript "1.10.866"]
+                                        [re-frame "1.2.0"]
+                                        [reagent "0.8.1"]
+                                        [re-catch "0.1.4"]
+                                        [devcards "0.2.6"]
+                                        [cljstache "2.0.6"]
+                                        [net.dhleong/spade "1.0.4"]]}})
 

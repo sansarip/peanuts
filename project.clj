2b452bdf66f7db526f9fb14d47fc277f27df2e81
sepehr ansaripour <pehrans@gmail.com>
2021-06-26 20:58:32 -0400

    :wrench: Add clj-kondo export resource path

--- project.clj
+++ project.clj
@@ -10,6 +10,7 @@
                  [org.clojure/test.check "1.0.0"]]
   :repl-options {:init-ns peanuts.core}
   :source-paths []
+  :resource-paths ["resources/clj-kondo"]
   :aliases {"fig:prod"       ["with-profile" "devcards" "run" "-m" "figwheel.main" "-bo" "prod"]
             "fig:dev"        ["with-profile" "devcards" "trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
             "deploy:clojars" ["with-profile" "peanuts" "deploy" "clojars"]
